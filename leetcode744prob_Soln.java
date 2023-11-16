4052e60980f53ebb802ab0b81cb54b32e95da53e
Viswanath Allam <vishwanath.allam@gmail.com>
2023-11-16 11:26:55 +0530

    leetcode 744Prob_2 diff return type approaches

--- leetcode744prob_Soln.java
+++ leetcode744prob_Soln.java
@@ -27,3 +27,23 @@ class Solution {
         return letters[start];
     }
 }
+
+
+/*or 2nd solution where we are returning start%letters.length value.
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start =0;
+        int end=letters.length-1;
+        while(start<=end){
+            int mid= start+(end-start)/2;
+            if(target<letters[mid]){
+                end=mid-1;
+            }
+            else {
+                start=mid+1;
+            }
+        }
+        return letters[start%letters.length];
+    }
+}
